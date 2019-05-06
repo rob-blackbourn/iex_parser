98021b183031f01a495f9e721906ba6c7e895dbe
Rob Blackbourn <rob.blackbourn@googlemail.com>
2019-05-06 08:27:23 +0100

    Tidied up examples

--- examples/read_deep.py
+++ examples/read_deep.py
@@ -1,10 +1,10 @@
-from iex_parser import Parser, DEEP_1_0, TOPS_1_6
+from iex_parser import Parser, DEEP_1_0
+import os
 
-DEEP_DATA_FILE = '/home/rtb/Data/iextrading.com/data_feeds_20190503_20190503_IEXTP1_DEEP1.0.pcap'
-DEEP_SAMPLE_DATA_FILE = '/home/rtb/Data/iextrading.com/data_feeds_20180127_20180127_IEXTP1_DEEP1.0.pcap'
-TOPS_SAMPLE_DATA_FILE = '/home/rtb/Data/iextrading.com/data_feeds_20180127_20180127_IEXTP1_TOPS1.6.pcap'
+DEEP_SAMPLE_DATA_FILE = os.path.join(os.environ['DATA_FOLDER'], 'data_feeds_20180127_20180127_IEXTP1_DEEP1.0.pcap')
+DEEP_DATA_FILE = os.path.join(os.environ['DATA_FOLDER'], 'data_feeds_20190503_20190503_IEXTP1_DEEP1.0.pcap')
 
-with Parser(TOPS_SAMPLE_DATA_FILE, TOPS_1_6) as reader:
+with Parser(DEEP_SAMPLE_DATA_FILE, DEEP_1_0) as reader:
     for message in reader:
         print(message)
 
